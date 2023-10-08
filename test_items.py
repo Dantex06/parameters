7d666bf11574aadf6f622d26f3b72703769eff7b
egorr <egorrogachev030@gmail.com>
2023-10-08 15:51:12 +0300

    time.sleep added to check

--- test_items.py
+++ test_items.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
+import time
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
     browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    time.sleep(30)
 
