cae3bf728eee5542b5a937f6652ec5e778fb9579
egorr <egorrogachev030@gmail.com>
2023-10-08 16:02:07 +0300

    rename function

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_button(browser):
     browser.get(link)
     button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
     assert button.is_displayed(), "Кнопка не найдена на странице"
