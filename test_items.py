e60a085d7b2412b1741ca6092762c2c2471ca2e3
egorr <egorrogachev030@gmail.com>
2023-10-08 15:59:09 +0300

    added assert

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 def test_guest_should_see_login_link_pass(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    button = browser.find_element(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert button.is_displayed(), "Кнопка не найдена на странице"
     time.sleep(30)
 
